d6a44c2c2d4ea35146cee981bb3edf3c848c3979
Herwig Stuetz <herwig@herwigstuetz.com>
2021-05-16 14:01:46 +0000

    Fix cpy cmake

--- tests/capi/cpy/cpy-config.cmake
+++ tests/capi/cpy/cpy-config.cmake
@@ -2,13 +2,16 @@ add_library(cpy STATIC IMPORTED GLOBAL)
 
 set_target_properties(cpy PROPERTIES
     IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../target/debug/libpy.a"
-#    IMPORTED_LINK_INTERFACE_LIBRARIES "-lpython3.8"
+    IMPORTED_LINK_INTERFACE_LIBRARIES "python3.8;pthread;dl"
+    INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../target/debug/")
 
 
 add_library(cpy-dynamic SHARED IMPORTED GLOBAL)
 
 set_target_properties(cpy-dynamic PROPERTIES
-    IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../target/debug/libpy.so"
+    # libpy.so is loaded explicitly
+    # IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../../target/debug/libpy.so"
+
     # python is loaded explicitly
     # IMPORTED_LINK_INTERFACE_LIBRARIES "-lpython3.8"
     INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../../target/debug/")
